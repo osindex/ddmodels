186bbb27c60bb836c42c5c4bcf0d188b55aa3788
osi <yaoiluo@gmail.com>
2018-04-11 00:46:58 +0800

    use json

--- src/Models/ExportData.php
+++ src/Models/ExportData.php
@@ -4,6 +4,9 @@ namespace Base\Models;
 class ExportData extends Model {
 	protected $table = "exportdata";
 	protected $guarded = [];
+	protected $casts = [
+		'options' => 'array',
+	];
 	public function admin() {
 		return $this->BelongsTo('Base\Models\Admin');
 	}
