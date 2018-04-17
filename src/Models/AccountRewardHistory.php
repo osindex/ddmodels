2f83d252a65336a9d2e70c63f2c79f31d74bf3ba
osi <yaoiluo@gmail.com>
2018-04-17 17:50:25 +0800

    add AccountRewardHistory InviteHistory relation

--- src/Models/AccountRewardHistory.php
+++ src/Models/AccountRewardHistory.php
@@ -5,4 +5,11 @@ class AccountRewardHistory extends Model {
 	// use Notifiable;
 	protected $table = "account_reward_histories";
 	protected $guarded = [];
+
+	public function account() {
+		return $this->BelongsTo('Base\Models\Account');
+	}
+	public function express() {
+		return $this->BelongsTo('Base\Models\Express');
+	}
 }
--- src/Models/InviteHistory.php
+++ src/Models/InviteHistory.php
@@ -5,4 +5,10 @@ class InviteHistory extends Model {
 	// use Notifiable;
 	protected $table = "invite_histories";
 	protected $guarded = [];
+	public function user() {
+		return $this->belongsTo('Base\Models\User', 'user_id');
+	}
+	public function invited_user() {
+		return $this->belongsTo('Base\Models\User', 'invited_user_id');
+	}
 }
