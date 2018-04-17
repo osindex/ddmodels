1daf7b55282f010f605b4c9bc95b52ef3d59c412
osi <yaoiluo@gmail.com>
2018-04-17 16:09:00 +0800

    add AccountRewardHistory InviteHistory

---
+++ src/Models/AccountRewardHistory.php
@@ -0,0 +1,8 @@
+<?php
+namespace Base\Models;
+
+class AccountRewardHistory extends Model {
+	// use Notifiable;
+	protected $table = "account_reward_histories";
+	protected $guarded = [];
+}
---
+++ src/Models/InviteHistory.php
@@ -0,0 +1,8 @@
+<?php
+namespace Base\Models;
+
+class InviteHistory extends Model {
+	// use Notifiable;
+	protected $table = "invite_histories";
+	protected $guarded = [];
+}
