f6ac2ac74a45bfde167d2de01cc325c703d7a413
osi <yaoiluo@gmail.com>
2018-05-15 09:14:12 +0800

    add expressaction

--- src/Models/Express.php
+++ src/Models/Express.php
@@ -73,6 +73,9 @@ class Express extends Model {
 	public function expressNopayTransaction() {
 		return $this->hasOne('Base\Models\ExpressTransaction')->where('state', 0)->where('type', 'pay');
 	}
+	public function expressAction() {
+		return $this->hasOne('Base\Models\ExpressAction');
+	}
 	public function expressCancle() {
 		return $this->hasOne('Base\Models\ExpressAction')->where('type', 'cancle')->orderBy('id', 'DESC');
 	}
