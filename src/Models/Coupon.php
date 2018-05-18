216c8304d3311c07afe18345ea8bc89c1e26acb7
osi <yaoiluo@gmail.com>
2018-05-19 00:55:01 +0800

    0.1.19

--- src/Models/Coupon.php
+++ src/Models/Coupon.php
@@ -12,4 +12,17 @@ class Coupon extends Model {
 	public function user() {
 		return $this->BelongsTo('Base\Models\User');
 	}
+	public static function get_unique_no($prefix = '', $length = 8, $channel = '', $strtofunc = false) {
+		$shortened = str_limit($prefix . (md5(microtime() . $channel . mt_rand(1, 10000))), $length, '');
+		$record = self::where('code', '=', $shortened)->first();
+		if (!empty($record)) {
+			// echo $shortened;
+			return static::get_unique_no();
+		} else {
+			if ($strtofunc) {
+				$shortened = call_user_func($strtofunc, $shortened);
+			}
+			return $shortened;
+		}
+	}
 }
